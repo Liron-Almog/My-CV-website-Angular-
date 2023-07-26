1bb64906d4bfb2b35b1b2fca279cd65e2973a9b8
Liron-Almog <liron_almoggg@gmail.com>
2023-07-26 14:43:18 +0300

    added icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { HomeHeaderComponent } from './home/home-header/home-header.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeMainComponent } from './home/home-main/home-main.component';
+import { HomeFooterComponent } from './home/home-footer/home-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HomeHeaderComponent,
+    NavBarComponent,
+    HomeMainComponent,
+    HomeFooterComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
