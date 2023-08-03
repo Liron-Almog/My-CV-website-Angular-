70a6abb1ced0568c62cdff09e1a95e848b9ef71f
Liron-Almog <liron_almoggg@gmail.com>
2023-08-03 18:48:07 +0300

    added coint hunter

--- src/app/my-projects/projects-list/projects-list.component.ts
+++ src/app/my-projects/projects-list/projects-list.component.ts
@@ -10,7 +10,15 @@ export class ProjectsListComponent implements OnInit{
   projects:Project[] = [new Project('Ecommerce','Test decription','Ecommerce.mp4'),
                         new Project('Memory Game','Test decription','Memory_Game.mp4'),
                         new Project('The Maze Game','Test decription','The_Maze.mp4'),
+                        new Project('Proxy-Tester-Console','Test decription',''),
+                        new Project('Circle-The-Cat','Test decription',''),
+                        new Project('Coin-Hunter','Test decription','Coin Hunter.mp4'),
+                        new Project('The-Prime-Numbers-Maker','Test decription',''),
+                        new Project('Sorting-Competiton','Test decription',''),
                         new Project('Trivia Game','Test decription','Trivia_Game.mp4')];
+
+                        
+
 
   constructor(){}
 
