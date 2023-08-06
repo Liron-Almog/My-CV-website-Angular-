114d7402a74c3c2e5287b3a343ad2917f2659d07
Liron-Almog <liron_almoggg@gmail.com>
2023-08-06 14:45:32 +0300

    added video to circle the cat

--- src/app/my-projects/projects-list/projects-list.component.ts
+++ src/app/my-projects/projects-list/projects-list.component.ts
@@ -10,14 +10,17 @@ export class ProjectsListComponent implements OnInit{
   projects:Project[] = [new Project('Ecommerce','HTML, CSS, JS & React','Ecommerce.mp4','https://github.com/Liron-Almog/Ecommerce-React'),
                         new Project('Memory Game','HTML, CSS & JS','Memory_Game.mp4','https://github.com/Liron-Almog/Memory-Game'),
                         new Project('The Maze','C++','The_Maze.mp4','https://github.com/Liron-Almog/The-Maze'),
-                        new Project('Proxy Tester Console','Java','','https://github.com/Liron-Almog/Proxy-Tester-Console'),
-                        new Project('Circle the Cat','C++','','https://github.com/Liron-Almog/Circle-The-Cat'),
+                        new Project('Trivia Game','HTML CSS & Java with spring','Trivia_Game.mp4','https://github.com/Liron-Almog/Trivia-Game-MVC-Thymeleaf'),
+                        new Project('Circle the Cat','C++','Circle_The_Cat.mp4','https://github.com/Liron-Almog/Circle-The-Cat'),
                         new Project('Coin Hunter','C++','Coin Hunter.mp4','https://github.com/Liron-Almog/Coin-Hunter'),
-                        new Project('Targeted Lotto Counter','C','','https://github.com/Liron-Almog/Targeted-Lotto-Counter'),
                         new Project('Cheaper App','React Native','','https://github.com/Liron-Almog/Cheaper-App'),
-                        new Project('Producer-Consumer','C','','https://github.com/Liron-Almog/Producer-Consumer-using-Named-Pipes'),
+                        new Project('Proxy Tester Console','Java','','https://github.com/Liron-Almog/Proxy-Tester-Console'),
+
                         new Project('Sorting Competiton','C','','https://github.com/Liron-Almog/Sorting-Competiton'),
-                        new Project('Trivia Game','HTML CSS & Java with spring','Trivia_Game.mp4','https://github.com/Liron-Almog/Trivia-Game-MVC-Thymeleaf')
+                        new Project('Targeted Lotto Counter','C','','https://github.com/Liron-Almog/Targeted-Lotto-Counter'),
+                        new Project('Producer-Consumer','C','','https://github.com/Liron-Almog/Producer-Consumer-using-Named-Pipes')
+                        
+                        
                       ];
 
                         
