43ff5bb50f300dacf92260d616c463aa816cf5c9
Liron-Almog <liron_almoggg@gmail.com>
2023-11-16 16:37:00 +0200

    added my cookbook project

--- src/app/my-projects/projects-explanation-list/projects-explanation-list.component.ts
+++ src/app/my-projects/projects-explanation-list/projects-explanation-list.component.ts
@@ -56,7 +56,7 @@ export class ProjectsExplanationListComponent {
                                               `This program demonstrates a simple producer-consumer scenario using named pipes. The producer generates random prime numbers and sends them through a named pipe.
                                                The consumer reads the numbers from the named pipe and prints them on the screen.`
           
-                                               ),new ProjectExplanation('Cheaper App',`In progress...`)
+                                               ),new ProjectExplanation('Cheaper App',`In progress...`),new ProjectExplanation('My Cookbook',`In progress...`)
 
 
 
